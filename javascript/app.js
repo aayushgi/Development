3b46e37b173661f4447de32fe11a295f8992b104
aayushgi <mrayushsaxena09@gmail.com>
2025-10-03 11:09:27 +0530

    add app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,80 @@
-console.log("hellow")
-let a= 6;
-let b= 5;
-console.log("sum is :", a+b)
+// console.log("hellow")
+// let a= 6;
+// let b= 5;
+// console.log("sum is :", a+b)
+ let pencil = 10;
+ let pen = 5;
+//  console.log("total is :", pencil+pen)
+let output = `the total prize is : ${pencil+pen} rupees`;
+console.log(output);
+//arithmatic operators
+let x = 10;
+let y = 5;
+let a = 5;
+console.log("the value of x+y is :", x+y);
+console.log("the value of x+y is :", x*y);
+console.log("the value of x+y is :", x-y);
+console.log("a++");
+console.log("++a");
+console.log("a++");
+console.log("++a");
+
+// comparison operator
+// let age = 15;
+// console.log(age>18)
+//in js every thing have their unique unicode
+'a'<'b'
+//conditional statment
+    //if statment
+    console.log("before my if statment:");
+    let age = 9;
+    if( age >=18){
+        console.log("eligible for voating")
+    }
+    if(age<18){
+        console.log("you are not eligible for voating") 
+    }
+    console.log("after my if statment:");
+
+
+    //nested if-else
+        let marks=96;
+    if(marks => 33){
+        console.log("pass")
+         if(marks>=80){
+            console.log("Grade A+")
+         }
+         else{
+            console.log("lower grade")
+         }
+    }
+    else{
+        console.log("better luck next time")
+    }
+
+    // logical operator
+    // && and
+    // || or
+    // ! not
+
+let marks1 = 97;
+if (marks1 >=33 && marks1 >=80){
+    console.log("passed")
+    console.log("Grade A+")
+}     
+
+
+//alerts and prompts
+// alert(display an alert message on the page)//it is used to show the message on the screen
+// prompt(it is used to take the input from the user)
+alert("hello this is an alert message")
+console.error("this is an error message")
+console.warn("this is a warning message")
+
+
+let fristName = prompt("plz enter your age")
+console.log(fristName)
+
+let fristname = prompt("plz enter your frist name")
+let lastname = prompt("plz enter your last name")
+console.log(fristname + " " + lastname)
