a76349dccbcfa9fe94903095d576189079d9070b
ayushgi <gitayush30@gmail.com>
2025-10-17 12:04:02 +0530

    new

--- javascript/array/array1.js
+++ javascript/array/array1.js
@@ -3,3 +3,5 @@ let students = ["aman", "sana", "janvi", "sahil", "akash", ]
 // we can add different different elements in array
 let random = [1, 2, "ayush", true, null, undefined, 3.14, "hello", "world", 42]
 // we can make changes in array but we can not change in string
+//arrays are mutable
+
