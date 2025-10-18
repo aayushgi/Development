a1300553800f013dd47aea90d4769229280b47bd
ayushgi <gitayush30@gmail.com>
2025-10-18 11:22:25 +0530

    new

--- javascript/array/array1.js
+++ javascript/array/array1.js
@@ -2,6 +2,24 @@
 let students = ["aman", "sana", "janvi", "sahil", "akash", ]
 // we can add different different elements in array
 let random = [1, 2, "ayush", true, null, undefined, 3.14, "hello", "world", 42]
+
 // we can make changes in array but we can not change in string
 //arrays are mutable
-
+let animal=["dog", "cat", "elephant"]
+// ____________________________________some common array methods____________________________________
+//we can add elements using push method
+// we can also remove elements using pop method
+// if we want to add or remove elements from starting of array we can use unshift and shift method
+// we are using  indexof method to find index of element
+// include methhod is used to check if element is present in array or not(it will be showing true or false)
+// concat method is used to merge two arrays
+let colors1 = ["red", "green", "blue"]
+let colors2 = ["yellow", "purple", "orange"]
+let allColors = colors1.concat(colors2)
+console.log(allColors) 
+console.log(students)
+// reverse method is used to reverse the array
+console.log(students.reverse())
+// we use slice method to get a portion of array
+console.log(students.slice(1,4))    
+// we use splice method to add or remove elements from array even we can replace the elements 
