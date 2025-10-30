5967a6feb76f60f7f8f23e9db0c2da1991c15e49
ayushgi <gitayush30@gmail.com>
2025-10-30 09:50:23 +0530

    adding multiple loops

--- javascript/loops/loop1.js
+++ javascript/loops/loop1.js
@@ -23,25 +23,25 @@ for(let i=2;  i<=50; i=i+2){
 }
 
 
-let n = prompt("Enter your number which table did you want to print");
-n = parseInt(n);  // this function we basically used to convert string into the interegers
-for(let i=n; i<=n*10; i=i+n){
-    console.log(i);
-}
+// let n = prompt("Enter your number which table did you want to print");
+// n = parseInt(n);  // this function we basically used to convert string into the interegers
+// for(let i=n; i<=n*10; i=i+n){
+//     console.log(i);
+// }
 
 
 // ___________________________________________________guess the favorite movie_____________________________________________
-let favoriteMovie = "pk";
-guessmovie = prompt("Guess my favorite movie");
-while((guessmovie !==favoriteMovie)&&(guessmovie!=="quit ")){
-     guessmovie=prompt("Wrong guess! Try again.");
+// let favoriteMovie = "pk";
+// guessmovie = prompt("Guess my favorite movie");
+// while((guessmovie !==favoriteMovie)&&(guessmovie!=="quit ")){
+//      guessmovie=prompt("Wrong guess! Try again.");
      
 
-}
+// }
 
-if(guessmovie === favoriteMovie){
-    console.log("Congratulations! You guessed it right.");
-}
+// if(guessmovie === favoriteMovie){
+//     console.log("Congratulations! You guessed it right.");
+// }
 // ______________________________________________________break keyword ______________________________________________________
 // we are using the break keyword to exit the loop when a certain condition is met
 let i=1;
@@ -53,4 +53,36 @@ while(i<=10){
     i++;
 
     
+}
+// _____________________________________________________loops with arrays_______________________________________________________
+let fruits = ["apple", "banana", "mango", "grapes", "orange"];
+fruits.push("pineapple")
+for(let i=0; i<fruits.length; i++){
+    console.log(i, fruits[i]);
+}
+
+// _______________________________________________nested loops with nested arrays_______________________________________________________
+let animals=[
+    ["cat", "dog", "cow"],
+    ["lion", "elephant", "tiger"]
+]
+for(let i=0; i<animals.length; i++){
+    console.log(i, animals[i]);
+    for(let j=0; j<animals[i].length; j++){
+        console.log("   ", j, animals[i][j]);
+    }
+}
+
+
+// __-_________________________________________________________for...of loop___________________________________________________________
+// this loop we are using when we have to acces the items of collections
+// this loop will not run on internet explorer brouser 
+let colors = ["red", "green", "blue", "yellow"];
+for(let color of colors){
+    console.log(color);
+}
+
+let heroes = [["superman", "batman", "spiderman"] , ["ironman" , "thor", "hulk"]]
+for(list of heroes){
+    console.log(List);
 }
