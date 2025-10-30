5ca6e8a1b94449f3eb6d53a600cd8ab77f1e0fbf
ayushgi <gitayush30@gmail.com>
2025-10-30 10:25:07 +0530

    add todo app

---
+++ javascript/To do app/TODO.js
@@ -0,0 +1,21 @@
+let todo=[];
+let req= prompt("plese enter your request");
+while(true){
+    if(req==="quit"){
+        console.log("quiting the app");
+        break;
+    }
+    if(req==="list"){
+        console.log("**********");
+        for(task of todo){
+            console.log(task);
+        }
+        console.log("**********");
+    }
+    else if(req == "add"){
+        let task= prompt("enter the task to be added");
+        todo.push(task);
+        console.log("task added");
+    }
+     req= prompt("plese enter your request");
+}
