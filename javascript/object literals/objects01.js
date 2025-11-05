3485a2564053bbf9ad3c8d90e82130cb2b4cf559
ayushgi <gitayush30@gmail.com>
2025-11-05 23:14:32 +0530

    get literals

--- javascript/object literals/objects01.js
+++ javascript/object literals/objects01.js
@@ -9,3 +9,15 @@ const product ={
     serial_number: 12345,
     price: 45000, 
 }
+// __________________________________________thread/twitwer post (creat an object leteral twiter post which include the folowing property)____
+ let twitter_post = {
+    username: "ayush_saxena",
+    content: "Learning javascript is fun!",
+    likes: 150,
+    repost: 20,
+    tags: ["javascript", "coding", "webdev"]
+ };
+
+ //  ____________________________________________get values__________________________________________
+ twitter_post.content; // "Learning javascript is fun!"
+ twitter_post.likes; // 150
