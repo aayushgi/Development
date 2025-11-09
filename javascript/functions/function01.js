c591431fe5eebc58bbc1914ed859dabf5d6b0a9e
ayushgi <gitayush30@gmail.com>
2025-11-09 23:19:28 +0530

    adding dice game

--- javascript/functions/function01.js
+++ javascript/functions/function01.js
@@ -1,1 +1,19 @@
 // no changes will apply today
+function printpoem(){
+    console.log("Roses are red, violets are blue,");
+    console.log("Sugar is sweet, and so are you.");
+
+}
+printpoem();
+// this is second problem we have to solve
+// 2. create a function to roll a dice and return a number between 1 to 6
+// math.random() used to create random number
+// math.floor() used to round of number to nearest integer
+function rolldice(){
+ let random= Math.floor(Math.random()*6)+1;
+ console.log(random);
+} 
+rolldice();
+rolldice();
+rolldice();
+rolldice();     //this will write to get the output ~
