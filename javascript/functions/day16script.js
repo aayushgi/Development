18db80f6a9a01671e5d81d1f513ad2565bccc3ba
ayushgi <gitayush30@gmail.com>
2025-11-12 23:12:51 +0530

    add

--- javascript/functions/day16script.js
+++ javascript/functions/day16script.js
@@ -2,4 +2,21 @@ function sum(a, b){
     return a + b;
 }
 let s = sum(2, 3);
-console.log(s)
+console.log(s) 
+// craete an function to return the sum of number from 1 to n
+function getsum(n){
+let sum = 0;
+for(let i=1; i<=n; i++){
+    sum += i;
+}
+return sum; 
+}
+// create an function that return the concatenation of two string in an array
+ let str = ["hello", "world", "javascript"];
+    function concatStrings(strArray){
+    let result = "";
+    for(let i=0; i<str.length; i++){
+    result += str[i];
+    }
+    return result;
+}
